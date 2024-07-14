103a250c7bb6106e9dd9797a71f063c8839d4349
Oleksandr Pavlyk <oleksandr.pavlyk03@gmail.com>
2024-07-14 15:22:28 +0300

    entity framework for db

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFramework.Core;
+using Notes.Domain;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+
+    }
+}
